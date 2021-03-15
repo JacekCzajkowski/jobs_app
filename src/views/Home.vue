67e815b9a4f30b694087b5defe65d079d7f18eb7
Jacek <ja_cek@gmx.com>
2021-03-15 19:34:07 +0100

    project update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore rem nemo cum adipisci reiciendis. Officia eius similique velit cupiditate. Voluptatem, necessitatibus iste doloribus nemo dolorem impedit dignissimos aspernatur laborum nam vel totam praesentium dolorum culpa et soluta hic rem dolor vero ex aperiam. Error, aliquid minus! Laboriosam, inventore magnam laborum esse eius quisquam alias animi ullam reiciendis. Ad quod porro sequi ipsum commodi ducimus explicabo tempora animi nihil. Perspiciatis animi tenetur quidem earum cum laudantium sequi illum, quibusdam consectetur amet, deleniti ullam. Mollitia inventore iure quod ut aliquid nesciunt? Beatae adipisci ullam nostrum tempore vel blanditiis ipsa earum est molestiae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore rem nemo cum adipisci reiciendis. Officia eius similique velit cupiditate. Voluptatem, necessitatibus iste doloribus nemo dolorem impedit dignissimos aspernatur laborum nam vel totam praesentium dolorum culpa et soluta hic rem dolor vero ex aperiam. Error, aliquid minus! Laboriosam, inventore magnam laborum esse eius quisquam alias animi ullam reiciendis. Ad quod porro sequi ipsum commodi ducimus explicabo tempora animi nihil. Perspiciatis animi tenetur quidem earum cum laudantium sequi illum, quibusdam consectetur amet, deleniti ullam. Mollitia inventore iure quod ut aliquid nesciunt? Beatae adipisci ullam nostrum tempore vel blanditiis ipsa earum est molestiae.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
